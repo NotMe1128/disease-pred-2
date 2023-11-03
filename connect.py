9cba1eff86f1e68527e3ab045b07a3b42a4dfe91
NotMe1128 <92991462+NotMe1128@users.noreply.github.com>
2023-11-03 20:41:42 +0530

    Add files via upload

--- connect.py
+++ connect.py
@@ -13,17 +13,21 @@ def hello(name=None):
 @app.route('/submitdata', methods=['POST'])
 def process_symptoms():
     global description,precaution1,precaution2,precaution3,precaution4
-    symptoms = request.json
-    separator=','
-    string_symptoms = separator.join(symptoms)
-    output1 = "You selected: " + ", ".join(symptoms)
-    predicted=disease.predictDisease(string_symptoms)
-    output=f"{predicted}"
-    disease1 = output
-    description,precaution=disease.get_disease(disease1,disease_list)
-    precaution1,precaution2,precaution3,precaution4=precaution[0],precaution[1],precaution[2],precaution[3]
-    return jsonify(output=output)
-
+    try:
+        symptoms = request.json
+        separator=','
+        string_symptoms = separator.join(symptoms)
+        output1 = "You selected: " + ", ".join(symptoms)
+        predicted=disease.predictDisease(string_symptoms)
+        output=f"{predicted}"
+        disease1 = output
+        description,precaution=disease.get_disease(disease1,disease_list)
+        precaution1,precaution2,precaution3,precaution4=precaution[0],precaution[1],precaution[2],precaution[3]
+        return jsonify(output=output)
+    except Exception as e:
+        app.logger.error(e)
+        return jsonify(msg="Sorry, The inputs you selected did not result in any predicted disease. Please make sure to select proper symptoms."), 500
+        print("error sent")
 @app.route('/results')
 def results():
     # get all the parameters from the query string
@@ -32,6 +36,10 @@ def results():
     
     # pass all the parameters to the template
     return render_template('result.html', output=output, description=description, precaution1=precaution1, precaution2=precaution2, precaution3=precaution3, precaution4=precaution4)
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify(msg="Sorry, The inputs you selected did not result in any predicted disease. Please make sure to select proper symptoms. "), 500
 
 if __name__ == '__main__':
     app.run()
